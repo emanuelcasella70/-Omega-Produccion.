1c0d3a1e78f6d9f178e5df8acc3aeecc87274b7e
Emanuel <emanuel.casellero@gmail.com>
2022-12-07 20:47:47 -0300

    actualizacion 2.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let boton = document.getElementById("id-button");
 let opacity = document.getElementById("opacity");
 
+
 boton.addEventListener("mouseenter", ()=> {opacity.style.opacity ="0.2";opacity.style.width= "100%";opacity.style.height="100%"; 
 opacity.style.backgroundColor="black"; opacity.style.position="absolute"; 
 });
@@ -8,12 +9,3 @@ opacity.style.backgroundColor="black"; opacity.style.position="absolute";
 boton.addEventListener("mouseleave", ()=> {opacity.style.opacity ="0"; 
 });
 
-
-const pito = {
-    type:"panda",
-    estado:"pito"
-}
-
-let {type} = pito
-
-console.log(type)
